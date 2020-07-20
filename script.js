e54752613393682e2cd3d43cf6bb9e57a2aca27d
Prisca Roseline <prisca.ros@onja.org>
2020-07-20 09:25:36 +0300

    submit the form bu using delegation method

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-console.log('good luck!');
+
+// REFERENCES
 const modalInner = document.querySelector('.modal-inner');
 const modalOuter = document.querySelector('.modal-outer');
 const addOrderBtn = document.querySelector('.add-order');
-const submitOrder = document.querySelector('.submit-order');
-const name = document.getElementById('name');
 const sel = document.querySelector('.select-form');
 const formElement = document.querySelector('form');
 const detailBtn= document.querySelector('.details')
 const deleteBtn = document.querySelector('.served');
-
 // Show the form
 
+
 const openModal = () => {
+
+  modalInner.innerHTML = `
+      <form>
+        <p>Your name :</p>
+        <input class="input-form" type="text" id="name" name="name" placeholder="Enter your name here" required />
+        <p>Please select your dish :</p>
+        <select name="dish" class="select-form" required>
+          <option value="romazava">Romazava</option>
+          <option value="koba">Koba</option>
+          <option value="ravitoto">Ravitoto</option>
+          <option value="mokary">Mokary</option>
+          <option value="achard">Achard</option>
+          <option value="masikita">Masikita</option>
+          <option value="sambos">Sambos</option>
+          <option value="mofo-baolina">Mofo baolina</option>
+          <option value="ranonapango">Ranonapango</option>
+        </select>
+        <p>Please select the size of your plate :</p>
+        <input type="radio" id="small" name="size" value="small" required />
+        <label for="small">Small</label><br />
+        <input type="radio" id="medium" name="size" value="medium" />
+        <label for="medium">Medium</label><br />
+        <input type="radio" id="large" name="size" value="large" />
+        <label for="large">Large</label><br />
+        <p>How many pieces do you want to order?</p>
+        <input class="input-form" type="number" id="quantity" name="amount" placeholder="Enter a number here"
+          required />
+        <button class="submit-order" type="submit">Add this order</button>
+    </form>
+  `
   modalOuter.classList.add('open');
 }
 
@@ -30,20 +59,28 @@ const handleEscape = (e) => {
 
 // Creat and show the new list
 
-submitOrder.addEventListener('click', (e) => {
+const subimitBtn = () => {
+  const name = document.getElementById('name');
   const myHtml = `<div class="order" data-dish="romazava" data-size="large" data-amount="2">
-  <span class="title">
-    ${name.value}
-  </span>
-  <button class="details">Details</button>
-  <button class="served">Delete order</button>
-  </div>
-</div>`
+    <span class="title">
+      ${name.value}
+    </span>
+    <button class="details">Details</button>
+    <button class="served">Delete order</button>
+    </div>
+  </div>`;
+  const orderList = document.querySelector('.order-list');
+  orderList.insertAdjacentHTML('beforeend', myHtml);
+  modalOuter.style.display = "none";
+};
 
-e.preventDefault();
-const orderList = document.querySelector('.order-list');
-orderList.insertAdjacentHTML("beforeend", myHtml);
-modalOuter.style.display = "none";
+// Listen the submit event
+
+window.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (event.target.matches('button.submit-order')) {
+    subimitBtn();
+  };
 });
 
 // Delete the list
@@ -82,6 +119,8 @@ const handleDelete = (e) => {
 //    }
 //    return '';
 // };
+
+// Listen the events
 
 window.addEventListener('keydown', handleEscape);
 modalOuter.addEventListener('click', handleClick);
